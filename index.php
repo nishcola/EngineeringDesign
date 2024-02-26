be8c2f5ce9d6a35560675ca039483dd39bb17443
nishcola <144401361+nishcola@users.noreply.github.com>
2024-02-26 17:57:52 -0500

    finished user account function

--- index.php
+++ index.php
@@ -1,5 +1,16 @@
+<?php
+session_start();
+if (isset($_SESSION["user_id"])) {
+    $mysqli = require __DIR__ . "/database.php";
+    $sql = "SELECT * FROM user
+            WHERE id = {$_SESSION["user_id"]}";
+    $result = $mysqli->query($sql);
+    $user = $result->fetch_assoc();
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -10,7 +21,18 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
+
 <body>
+    <nav class="navbar bg-body-tertiary">
+        <div class="container-fluid">
+            <?php if (isset($_SESSION["user_id"])): ?>
+                <span class="navbar-brand mb-0 h1">Welcome, <?= htmlspecialchars($user["username"]) ?></span>
+            <?php else: ?>
+                <span class="navbar-brand mb-0 h1">You are not logged in. <a href="login.php">Log in</a> or <a
+                        href="signup.html">sign up.</a></span>
+            <?php endif; ?>
+        </div>
+    </nav>
     <div class="container mt-4">
         <h2 class="mb-3">Report History</h2>
         <hr>
@@ -51,7 +73,10 @@
             </tbody>
         </table>
         <button class="btn btn-primary">Create New</button>
-        <button class="btn btn-danger">Log Out</button>
+        <?php if (isset($_SESSION["user_id"])): ?>
+            <a href="logout.php"><button class="btn btn-danger">Log Out</button></a>
+        <?php endif; ?>
     </div>
 </body>
+
 </html>
--- login.php
+++ login.php
@@ -14,7 +14,11 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
 
   if ($user) {
       if (password_verify($_POST["password"], $user["password_hash"])) {
-        die("Login successful!");
+        session_start();
+        session_regenerate_id();
+        $_SESSION["user_id"] = $user["id"];
+        header("Location: index.php");
+        exit;
       }
   }
 
